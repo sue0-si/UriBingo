52948c12e763461e9d2f3de041c4aea3e206104a
Kimdahee <dh980502@gmail.com>
2024-01-22 18:12:57 +0900

    main page ui 수정

--- lib/view/page/main_screen.dart
+++ lib/view/page/main_screen.dart
@@ -10,16 +10,15 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   List<Widget> fridges = [];
 
-  Widget makeFridge() {
+  Widget makeFridge(int index) {
     return Container(
       width: 100,
-      height: 200,
+      height: 150,
       decoration: BoxDecoration(
-        border: Border.all(),
-      ),
+          border: Border.all(), borderRadius: BorderRadius.circular(10)),
       child: Center(
         child: Text(
-          '1번 냉장고',
+          '${index+1}번 냉장고',
         ),
       ),
     );
@@ -29,23 +28,35 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('공용 냉장고')),
+        title: Center(
+          child: Text('공용 냉장고'),
+        ),
       ),
-      body: Row(
-        children: [
-          ...fridges,
-          IconButton(
-            onPressed: () {
-              setState(() {
-                fridges.add(makeFridge());
-              });
-            },
-            icon: Icon(
-              Icons.add,
-              size: 50,
-            ),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 30,
+              crossAxisSpacing: 30,
+              childAspectRatio: 4 / 6),
+          itemCount: fridges.length + 1,
+          itemBuilder: (context, index) {
+            if (index == fridges.length) {
+              // Last item is the add button
+              return IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () {
+                  fridges.add(makeFridge(index));
+                  setState(() {});
+                },
+              );
+            } else {
+              // Display the fridge container
+              return fridges[index];
+            }
+          },
+        ),
       ),
     );
   }
