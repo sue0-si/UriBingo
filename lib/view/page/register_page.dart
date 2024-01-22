8091b702e25af1dc282b6ba7c68561e22ea2d3f2
slee3593 <slee3593@gatech.edu>
2024-01-22 12:52:42 +0900

    feat: 카메라 연동해서 찍은 사진 화면에 띄우기

--- lib/view/page/register_page.dart
+++ lib/view/page/register_page.dart
@@ -12,45 +12,60 @@ class _RegisterPageState extends State<RegisterPage> {
   final ImagePicker picker = ImagePicker();
   XFile? photo;
 
-  pickImage() async {
+  Future<void> pickImage() async {
     photo = await picker.pickImage(source: ImageSource.camera);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    pickImage();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          // photo == null
-          //     ? const Center(
-          //         child: Text('No data'),
-          //       )
-          //     : FutureBuilder(
-          //         future: photo?.readAsBytes(),
-          //         builder: (context, snapshot) {
-          //           final data = snapshot.data;
-          //           if (data == null ||
-          //               snapshot.connectionState == ConnectionState.waiting) {
-          //             return const Center(
-          //               child: CircularProgressIndicator(),
-          //             );
-          //           }
-          //           return Image.memory(
-          //             data,
-          //             width: double.infinity,
-          //           );
-          //         },
-          //       )
-          Container(
-            height: 50,
-            color: Colors.grey,
-          ),
-          ElevatedButton(onPressed: () {}, child: Text('취소')),
-          ElevatedButton(onPressed: () {}, child: Text('등록하기')),
-          ElevatedButton(onPressed: () {}, child: Text('공용')),
-          ElevatedButton(onPressed: () {}, child: Text('미확인')),
-        ],
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                pickImage();
+              },
+              child: photo == null
+                  ? Container(
+                      height: MediaQuery.of(context).size.height / 2,
+                      color: Colors.grey,
+                    )
+                  : FutureBuilder(
+                      future: photo?.readAsBytes(),
+                      builder: (context, snapshot) {
+                        final data = snapshot.data;
+                        if (data == null ||
+                            snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return Image.memory(
+                          data,
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height / 2,
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text('취소')),
+            ElevatedButton(onPressed: () {}, child: Text('등록하기')),
+            ElevatedButton(onPressed: () {}, child: Text('공용')),
+            ElevatedButton(onPressed: () {}, child: Text('미확인')),
+          ],
+        ),
       )),
     );
   }
