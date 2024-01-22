a08224052209caf87d24d4e43311b7b98456798c
Sangin Han <hugedreamhd@gmail.com>
2024-01-22 18:34:26 +0900

    feat: dropdownbutton 사이즈 조절 및 냉장고 이름 textField 사이즈 조절 fail

--- lib/view/page/main_page.dart
+++ lib/view/page/main_page.dart
@@ -11,8 +11,8 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  final _numberOfCompartments = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  int _selectedCompartments = 0;
+  final _numberOfCompartments = ['1칸', '2칸', '3칸', '4칸', '5칸'];
+  String _selectedCompartments = '';
 
   @override
   void initState() {
@@ -28,51 +28,49 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         actions: [],
       ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 30),
-                child: Container(
-                  height: 300,
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                      
-                        children: [
-                          Expanded(
-                            child: const Text(
-                              '냉장고 이름',
-                              style: TextStyle(
-                                fontSize: 25,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Container(
+                    height: 300,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Expanded(
+                              flex: 1,
+                              child: Text(
+                                '냉장고 이름',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
                               ),
                             ),
-                          ),
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              width: 150,
-                              height: 40,
+                            Expanded(
+                              flex: 1,
                               child: TextField(
+                                style: TextStyle(fontSize: 15),
                                 decoration: InputDecoration(
                                   focusedBorder: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(10),
                                     borderSide: const BorderSide(
-                                      width: 3,
+                                      width: 1,
                                       color: Colors.green,
                                     ),
                                   ),
                                   enabledBorder: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(10),
                                     borderSide: const BorderSide(
-                                      width: 3,
+                                      width: 1,
                                       color: Colors.green,
                                     ),
                                   ),
@@ -80,137 +78,170 @@ class _MainPageState extends State<MainPage> {
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-                      // 이 부분을 수정하여 두 번째 냉장고 이름을 표시하세요.
-                       Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              '냉장고 칸수',
-                              style: TextStyle(
-                                fontSize: 25,
+                            SizedBox(
+                              width: 10,
+                            ),
+                          ],
+                        ),
+                        // 이 부분을 수정하여 두 번째 냉장고 이름을 표시하세요.
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Expanded(
+                              flex: 1,
+                              child: Text(
+                                '냉장고 칸수',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
                               ),
                             ),
-                          ),
-                          Expanded(
-                            child: DropdownButton(
-                                value: _selectedCompartments,
-                                items: _numberOfCompartments
-                                    .map((e) => DropdownMenuItem(
-                                          value: e,
-                                          child: Text(e.toString()),//int를 String으로 변환
-                                        )).toList(),
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedCompartments = value!;
-                                  });
-                                }),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              '냉동고 칸수',
-                              style: TextStyle(
-                                fontSize: 25,
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                margin: EdgeInsets.only(right: 40),
+                                child: DropdownButton(
+                                    value: _selectedCompartments,
+                                    items: _numberOfCompartments
+                                        .map((e) => DropdownMenuItem(
+                                              value: e,
+                                              child: Text(e
+                                                  .toString()), //int를 String으로 변환
+                                            ))
+                                        .toList(),
+                                    isExpanded: true,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _selectedCompartments = value!;
+                                      });
+                                    }),
                               ),
                             ),
-                          ),
-                          Expanded(
-                            child: DropdownButton(
-                                value: _selectedCompartments,
-                                items: _numberOfCompartments
-                                    .map((e) => DropdownMenuItem(
-                                  value: e,
-                                  child: Text(e.toString()),//int를 String으로 변환
-                                )).toList(),
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedCompartments = value!;
-                                  });
-                                }),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              '보관기간',
-                              style: TextStyle(
-                                fontSize: 25,
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Expanded(
+                              flex: 1,
+                              child: Text(
+                                '냉동고 칸수',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
                               ),
                             ),
-                          ),
-                          Expanded(
-                            child: DropdownButton(
-                                value: _selectedCompartments,
-                                items: _numberOfCompartments
-                                    .map((e) => DropdownMenuItem(
-                                  value: e,
-                                  child: Text(e.toString()),//int를 String으로 변환
-                                )).toList(),
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedCompartments = value!;
-                                  });
-                                }),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              '연장가능기간',
-                              style: TextStyle(
-                                fontSize: 25,
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                margin: EdgeInsets.only(right: 40),
+                                child: DropdownButton(
+                                    value: _selectedCompartments,
+                                    items: _numberOfCompartments
+                                        .map((e) => DropdownMenuItem(
+                                              value: e,
+                                              child: Text(e), //int를 String으로 변환
+                                            ))
+                                        .toList(),
+                                    isExpanded: true,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _selectedCompartments = value!;
+                                      });
+                                    }),
                               ),
                             ),
-                          ),
-                          Expanded(
-                            child: DropdownButton(
-                                value: _selectedCompartments,
-                                items: _numberOfCompartments
-                                    .map((e) => DropdownMenuItem(
-                                  value: e,
-                                  child: Text(e.toString()),//int를 String으로 변환
-                                )).toList(),
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedCompartments = value!;
-                                  });
-                                }),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text(
-                      '추가하기',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                      ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Expanded(
+                              flex: 1,
+                              child: Text(
+                                '보관기간',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                margin: EdgeInsets.only(right: 40),
+                                child: DropdownButton(
+                                    value: _selectedCompartments,
+                                    items: _numberOfCompartments
+                                        .map((e) => DropdownMenuItem(
+                                              value: e,
+                                              child: Text(e), //int를 String으로 변환
+                                            ))
+                                        .toList(),
+                                    isExpanded: true,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _selectedCompartments = value!;
+                                      });
+                                    }),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const Expanded(
+                              child: Text(
+                                '연장가능기간',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                margin: EdgeInsets.only(right: 40),
+                                child: DropdownButton(
+                                    elevation: 10,
+                                    dropdownColor: Colors.green,
+                                    value: _selectedCompartments,
+                                    items: _numberOfCompartments
+                                        .map((e) => DropdownMenuItem(
+                                              value: e,
+                                              child: Text(e), //int를 String으로 변환
+                                            ))
+                                        .toList(),
+                                    isExpanded: true,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _selectedCompartments = value!;
+                                      });
+                                    }),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text(
+                        '추가하기',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
