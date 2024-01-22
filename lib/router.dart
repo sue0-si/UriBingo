d331089db05d1fea4a8a3b434dfdecc0b4d84d3b
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-22 14:06:52 +0900

    feat: UI 완성

--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,9 @@
 import 'package:go_router/go_router.dart';
-import 'package:leute/view/page/main_page.dart';
+import 'package:leute/view/page/my_refrige_page.dart';
 
 final router = GoRouter(initialLocation: '/', routes: [
   GoRoute(
     path: '/',
-    builder: (context, state) => const MainPage(),
+    builder: (context, state) =>  MyRefrigePage(registerDate: DateTime.now().toLocal().toString(), remainPeriod: 02,),
   ),
 ]);
