123167bb7e57db3fbc1bba5ff5f43569daa8f011
ssd72 <sungdae.yi@gmail.com>
2024-01-24 15:21:55 +0100

    add: 상인님, 냉장고추가페이지 작성

--- lib/router.dart
+++ lib/router.dart
@@ -7,6 +7,8 @@ import 'package:leute/view/page/main_screen_delete.dart';
 import 'package:leute/view/page/my_food_detail_page/my_food_detail.dart';
 import 'package:leute/view/page/login_pages/login_page.dart';
 import 'package:leute/view/page/login_pages/signup_page.dart';
+import 'package:leute/view/page/refrige_pages/add_refrige_new.dart';
+import 'package:leute/view/page/refrige_pages/add_refrige_new_view_model.dart';
 import 'package:leute/view/page/register_page/register_page.dart';
 import 'view/page/refrige_detail_page/refrige_detail_screen.dart';
 import 'view/page/refrige_pages/add_refrige.dart';
@@ -48,9 +50,11 @@ final router = GoRouter(initialLocation: '/login', routes: [
   ),
 
   GoRoute(
-    path: '/addRefrige',
-    builder: (context, state) => const addRefrige(),
-  ),
+      path: '/addRefrige',
+      builder: (context, state) => ChangeNotifierProvider(
+            create: (_) => AddRefrigeViewModel(),
+            child: const AddRefrige(),
+          )),
 
   GoRoute(
     path: '/addMyFood',
