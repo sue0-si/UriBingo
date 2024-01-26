5cba46c03f46977eb9936b2414daea85ead2ec22
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-26 14:42:36 +0900

    데이터베이스 isExtend 값 수정되도록 변경

--- lib/view_model/my_food_detail_view_model.dart
+++ lib/view_model/my_food_detail_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:leute/data/models/foods_model.dart';
 import 'package:leute/data/models/refrige_model.dart';
@@ -8,8 +9,15 @@ class MyFoodDetailViewModel with ChangeNotifier {
 
   int calculateRemainPeriod(FoodDetail myFoodItem, RefrigeDetail ourRefrigeItem) {
     int passedDate =
-        DateTime.now().difference(DateTime.fromMillisecondsSinceEpoch(myFoodItem.registerDate)).inDays;
+        DateTime
+            .now()
+            .difference(
+            DateTime.fromMillisecondsSinceEpoch(myFoodItem.registerDate))
+            .inDays;
     int remainPeriod = ourRefrigeItem.period - passedDate;
+    // if (myFoodItem.isExtended == true) {
+    //   remainPeriod += ourRefirgeItem.period;
+    // }
     return remainPeriod;
   }
 
@@ -20,9 +28,13 @@ class MyFoodDetailViewModel with ChangeNotifier {
     return extendedPeriod = remainPeriod + ourRefirgeItem.period;
   }
 
+
   bool checkOld(FoodDetail myFoodItem, RefrigeDetail ourRefirgeItem) {
     int remainPeriod = calculateRemainPeriod(myFoodItem, ourRefirgeItem);
-    return remainPeriod > 2;
+    // if (myFoodItem.isExtended == true) {
+    //   checkOld == false;
+    // }
+    return remainPeriod < 2;
   }
 
 }
