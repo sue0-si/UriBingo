2f7d5d7ef94afc5cb63b0fa7c70479fdb70d8401
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-26 22:25:05 +0900

    위젯추가

---
+++ lib/view/widget/login_widget/advertising_area.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AdvertisingArea extends StatelessWidget {
+  const AdvertisingArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 100.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+        color: Colors.white,
+      ),
+      child: const Center(
+        child: Text(
+          '광고',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view_model/group_setting_page_view_model.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class GroupSettingPageViewModel with ChangeNotifier {}
