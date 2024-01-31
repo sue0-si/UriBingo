33d65347e284f56a8f289a41e30778061e36acfe
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-01-31 12:38:40 +0900

    다이얼로그 디자인 추가

--- lib/view/widget/custom_dialog/one_answer_dialog.dart
+++ lib/view/widget/custom_dialog/one_answer_dialog.dart
@@ -1,26 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../styles/app_text_style.dart';
 
 class OneAnswerDialog extends StatelessWidget {
   final Function() onTap;
   final String title;
+  final String subtitle;
   final String firstButton;
 
   const OneAnswerDialog(
       {super.key,
-      required this.onTap,
-      required this.title,
-      required this.firstButton});
+        required this.onTap,
+        required this.title,
+        required this.subtitle,
+        required this.firstButton});
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text(title),
-      actions: <Widget>[
-        ElevatedButton(
-          onPressed: onTap,
-          child: Text(firstButton),
+    return Dialog(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
         ),
-      ],
+        height: 180.h,
+        width: 270.w,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/dialog_cookie.gif',
+              width: 80,
+              height: 80,
+            ),
+            Text(
+              title,
+              style: AppTextStyle.body18M(),
+            ),
+            SizedBox(height: 6.0.h),
+            Text(
+              subtitle,
+              style: AppTextStyle.body12R(),
+            ),
+            SizedBox(height: 16.0.h),
+            ElevatedButton(
+                onPressed: onTap,
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF8BC6CC)),
+                child: Text(firstButton,
+                    style: AppTextStyle.body12R(color: Colors.white))),
+          ],
+        ),
+      ),
     );
   }
 }
