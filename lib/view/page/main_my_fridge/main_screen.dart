807ce9ff80f7908c4cb97655058c694e9ce0e9bd
Kimdahee <dh980502@gmail.com>
2024-01-31 11:34:17 +0900

    메인스크린 ui 수정

--- lib/view/page/main_my_fridge/main_screen.dart
+++ lib/view/page/main_my_fridge/main_screen.dart
@@ -71,7 +71,7 @@ class _MainScreenState extends State<MainScreen> {
                               ),
                               onPressed: () {
                                 context.go('/addRefrige', extra: currentUser);
-                                },
+                              },
                             )
                           : Container();
                     } else {
