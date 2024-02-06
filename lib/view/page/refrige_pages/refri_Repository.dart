0e64609c108a95606f40d38d267d70ab2fd706e8
Sangin Han <hugedreamhd@gmail.com>
2024-02-06 09:31:18 +0900

    2월6일 오전 push

---
+++ lib/view/page/refrige_pages/refri_Repository.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+abstract interface class RefriRepository {
+  Future<List<>>
+
+}
+void diSetup() {
+  //싱글턴
+  getIt.registerSingleton<instance>
+  
+}
